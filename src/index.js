a491491d21f9e8c718775d0efb1171f3e85ea1ef
Anton Maryn <tonysmaryn@gmail.com>
2020-10-03 20:46:57 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof(array) !== 'object' || array.length === 0 ){
+    return 0;
+  }
+  let min = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (min > array[i]){
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof(array) !== 'object' || array.length === 0 ){
+    return 0;
+  }
+  let max = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (max < array[i]){
+      max = array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof(array) !== 'object' || array.length === 0 ){
+    return 0;
+  }
+  let sum = 0;
+  let i = 0;
+  for (i; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum/i;
 }
